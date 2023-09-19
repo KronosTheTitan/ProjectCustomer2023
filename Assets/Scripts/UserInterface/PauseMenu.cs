2e661dcb99f6d8a8b37d9f6fc4a5dba5e30c13b2
Tjalle Borgers <tjalle@borgersonline.nl>
2023-09-19 15:53:02 +0200

    Pause menu

--- Assets/Scripts/UserInterface/PauseMenu.cs
+++ Assets/Scripts/UserInterface/PauseMenu.cs
@@ -1,7 +1,32 @@
-﻿namespace UserInterface
+﻿using Managers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UserInterface
 {
-    public class PauseMenu
+    public class PauseMenu : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject mainUIObject;
+        [SerializeField] private GameObject pauseMenuObject;
+
+        public void Open()
+        {
+            Time.timeScale = 0;
+            mainUIObject.SetActive(false);
+            pauseMenuObject.SetActive(true);
+        }
+
+        public void Close()
+        {
+            Time.timeScale = 1;
+            mainUIObject.SetActive(true);
+            pauseMenuObject.SetActive(false);
+        }
+
+        public void Resign()
+        {
+            GameManager.GetInstance().GameOver();
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 }
